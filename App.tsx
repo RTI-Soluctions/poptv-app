b5a3fed428087e767b7a65efe9a8b9b7e0dfa745
Ricardo Martins <102545943+ormartins01@users.noreply.github.com>
2026-07-05 22:29:19 -0300

    feat: implement new video player with refresh capabilities, network monitoring, and screen keep-awake functionality

--- App.tsx
+++ App.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Home } from "./src/app/Home";
 import { MyContextProvider } from "./src/context/AppContext";
+import { useKeepAwake } from "expo-keep-awake";
 
 export default function App() {
+  useKeepAwake(); // mantém a tela ligada enquanto o app estiver em primeiro plano
+
   return (
     <MyContextProvider>
       <Home />
